f4a04f6610350806a4f1d0a244e8457d8191bbaa
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-03-01 14:37:48 +0530

    feat: add Highlighter component for enhanced text highlighting and update package dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { VideoText } from "./ui/VideoText";
 import galaxyVideo from "../assets/galaxy.mp4";
+import { Highlighter } from "./ui/Highlighter";
 
 export default function Hero() {
   return (
@@ -24,9 +25,19 @@ export default function Hero() {
         
         <div className="flex justify-end mt-3 md:mt-5">
           <p className="max-w-[630px] md:text-xl text-[2rem] text-white/65 font-bold leading-snug text-start">
-            i build dynamic web apps , automating them through agents and giving
-            life through visuals. enthusiastic about creating seamless user
-            experiences.
+            i build{" "}
+            <Highlighter color="#3b82f6" padding={4} isView={true}>
+              dynamic web apps
+            </Highlighter>
+            , automating them through{" "}
+            <Highlighter color="#10b981" padding={4} isView={true}>
+              agents
+            </Highlighter>
+            {" "}and giving life through{" "}
+            <Highlighter color="#f59e0b" padding={4} isView={true}>
+              visuals
+            </Highlighter>
+            . enthusiastic about creating seamless user experiences.
           </p>
         </div>
       </div>
